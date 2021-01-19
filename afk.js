43a2753e4552ec9572c35a5c2bdf60ce3ae83b5e
OscarGamer2017 <72371862+OscarGamer2017@users.noreply.github.com>
2021-01-19 14:21:19 -0600

    ayuda

---
+++ afk.js
@@ -0,0 +1,55 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({ws: {Intents: new Discord.Intents(Discord.Intents.ALL)}});
+const config = require("./config.json");
+let prefix = config.prefix;
+
+Array.prototype.romove = function(x){
+    return this.filter(function(v){
+     return v !== x;
+    });
+   };
+   
+   client.on("error", (e) => console.error(e));
+   
+   client.on("ready", () => {
+      console.log(`afk esta listo!,En ${client.user.tag}`)
+   
+   });
+client.on("message", async message => {
+    if(message.author.bot) return;
+    if(!message.guild) return;
+    if(!message.content.startsWith(config.prefix)) return;
+    var fullmsg = message.content.split(" ");
+    var command = fullmsg [0].replace(config.prefix, "");
+    var args = fullmsg.romove(fullmsg[0]);
+    if (!command) return; 
+
+    if (message.member.displayName.startsWith(`[AFK]`)) return;
+  let user = message.author;
+  let avatar = user.avatarURL({ dynamic: true, format: `png`, size: 2048 });
+  let content = args.join(" ");
+  if (!content) {
+    content = "Ninguno";
+  }
+  
+  if(command ===  `afk`) { 
+            if (!message.member.permissionsIn(message.channel).has('ADMINISTRATOR')) {
+      return message.channel.send(`${message.author.username} , **no puedes hacer esto no eres moderador**`)}
+    const user = message.mentions.users.first() || message.author;
+      const embed1 = new Discord.MessageEmbed()
+      
+      .setAuthor(`${user.username} estara afk`)
+      .setDescription(`**Tiempo y Motivo:** \n ${content}`)
+      .setThumbnail(user.avatarURL({ dynamic: true, format: `png`, size: 2048 }))
+      .setFooter(`afk en ${message.guild.name}`)
+      .setTimestamp()
+      .setColor(0xff0000)
+  message.channel.send(embed1);
+  message.delete()
+  var afk_user = await message.guild.members.cache.get(message.author.id);
+  var nombre = afk_user.displayName;
+  		if(nombre && `[AFK] ${nombre}`.length <= 32) {
+			afk_user.setNickname(`[AFK] ${nombre}`).catch(()=>{});
+		}
+} });
+client.login(config.token);
---
+++ say.js
@@ -0,0 +1,84 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({ws: {Intents: new Discord.Intents(Discord.Intents.ALL)}});
+const config = require("./config.json");
+const fs = require("fs");
+const fse = require("fs-extra");
+const { disconnect } = require("cluster");
+let prefix = config.prefix;
+
+Array.prototype.romove = function(x){
+    return this.filter(function(v){
+     return v !== x;
+    });
+   };
+   
+   client.on("error", (e) => console.error(e));
+   
+   client.on("ready", () => {
+      console.log(`say esta listo!,En ${client.user.tag}`)
+   
+});
+
+client.on("message", function (message){
+
+})
+
+
+client.on("message", async message => {
+         if(message.author.bot) return;
+         if(!message.guild) return;
+         if(!message.content.startsWith(config.prefix)) return;
+         var fullmsg = message.content.split(" ");
+         var command = fullmsg [0].replace(config.prefix, "");
+         var args = fullmsg.romove(fullmsg[0]);
+         console.log(`${command} ${args.join(" ")}`);
+         if (!command) return; 
+if(command ===  "say") { 
+              if (!message.member.permissionsIn(message.channel).has('ADMINISTRATOR')) {
+      return message.channel.send(`${message.author.username} , **no puedes hacer esto no eres moderador**`)}
+   message.delete();
+    if(!args[0]) return message.channel.send("Tambien escribe lo que quieres que diga");
+    
+const texto = args.map(p=>{
+
+if (message.member.hasPermission('ADMINISTRATOR')) {
+return p;
+
+} else if (p=="@everyone"&&!message.member.hasPermission("MENTION_EVERYONE")) {
+return "everyone";
+
+} else if (p=="@here"&&!message.member.hasPermission("MENTION_EVERYONE")){
+return "here";
+
+} else if(p.startsWith("<@&")&& p.endsWith(">")) {
+
+let rol = message.guild.roles.cache.get(p.substring(3, p.length-1));
+
+if (!rol) {
+ return p;
+
+} else if (rol.mentionable) {
+ return p;
+
+} else {
+ return rol.name;
+}
+} else {
+return p;
+}
+});
+
+if(!args[0].match(/<#(\d{17,19})>/)){
+message.channel.send(texto.join(' '));
+
+}else{
+
+if(!args[1]) return message.channel.send("Tambien escribe lo que quieres que diga")
+
+const canal = message.mentions.channels.first();
+
+if (!canal) return message.channel.send("Canal no encontrado");
+
+canal.send(texto.slice(1).join(' '));}}
+});
+client.login(config.token);
